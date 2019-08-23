869a294121d4c4828ecdd8a869aae9029689f68d
mrkhavu <mrkhavu@gmail.com>
2019-08-23 18:53:37 +0700

    update source

--- chs_football_js.js
+++ chs_football_js.js
@@ -1,5 +1,5 @@
-var top_1 = ['Quân Phạm', 'Chú Phương', 'Nơ','Linh Tăng', 'Trang','Nái', 'Đình', 'Văn', 'Duy', 'Điệp','Vũ', 'Phượng', 'Tiên'];
-var top_2 = ['kha', 'Phương Linh'];
+var top_1 = ['Quân Phạm', 'Chú Phương', 'Nơ','Linh Tăng','Nái', 'Đình', 'Văn', 'Duy', 'Điệp','Vũ', 'Phượng', 'Tiên'];
+var top_2 = ['kha', 'Phương Linh', 'Trang'];
 var player_total = "";
 var top_num = "";
 var text = "";
@@ -14,7 +14,7 @@ function list_view(){
 	}
 	text = text + "</ul>";
 	document.getElementById("chs_list_view").innerHTML = text;
-	document.getElementById("chs_player_total").innerHTML = "Có " + player_total + " bé đang chờ, để được chọn =))";
+	// document.getElementById("chs_player_total").innerHTML = "Có " + player_total + " bé đang chờ, để được chọn =))";
 }
 function delete_item(i, j){
 	j.splice(i,1);
@@ -37,24 +37,41 @@ function myFunction() {
 
 var x;
 function count_down(){
-	var num_random = Math.floor(Math.random() * top_1.length);
-	document.getElementById("chs_popup_content").innerHTML = "START";
-	var countDownDate = new Date().getTime();// + 4200;
-	x = setInterval(function() {
-		var now = new Date().getTime();
-		var distance = countDownDate - now;
-		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-		document.getElementById("chs_popup_content").innerHTML = seconds;
-		if (distance < 0) {
+	var list_random = 0;
+	x = setInterval(function(){
+		var num_random = Math.floor(Math.random() * top_1.length);
+		if(list_random < 20){
+			document.getElementById("chs_popup_content").innerHTML = top_1[num_random];
+			list_random++;
+		}else{
 			clearInterval(x);
 			top_2.push(top_1[num_random]);
 			document.getElementById("chs_popup_content").innerHTML = top_1[num_random];
+			delete_item(num_random, top_1);
+			show_list_select();
 		}
-		delete_item(num_random, top_1);
-		show_list_select();
-	}, 1000);
+
+	}, 150);
+
+
+
+	// document.getElementById("chs_popup_content").innerHTML = "START";
+	// var countDownDate = new Date().getTime();// + 4200;
+	// x = setInterval(function() {
+	// 	var now = new Date().getTime();
+	// 	var distance = countDownDate - now;
+	// 	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	// 	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	// 	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	// 	document.getElementById("chs_popup_content").innerHTML = seconds;
+	// 	if (distance < 0) {
+	// 		clearInterval(x);
+	// 		top_2.push(top_1[num_random]);
+	// 		document.getElementById("chs_popup_content").innerHTML = top_1[num_random];
+	// 	}
+		// delete_item(num_random, top_1);
+		// show_list_select();
+	// }, 1000);
 }
 function stop_count_down(){
 	clearInterval(x);
@@ -64,7 +81,6 @@ function show_list_select(){
 	for (i = 0; i < top_2.length; i++){
 		text_select += "<li class='chs_list_2team'>" + top_2[i] + "</li>";
 	}
-
 	text_select = text_select + "</ul>";
     document.getElementById("chs_list_select").innerHTML = text_select;
 }
